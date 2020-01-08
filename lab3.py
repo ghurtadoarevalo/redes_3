4d6eea8d97a99f85e650d47d2e0398d631dd2a02
mcoronado <matias.coronado@usach.cl>
2020-01-08 00:34:15 -0300

    pyplox function's

--- lab3.py
+++ lab3.py
@@ -1,9 +1,12 @@
 from skimage import io
 from matplotlib import pyplot as plt
 import numpy as np
-kernel = np.array([[256,256,256],[256,256,256],[256,256,256]])
-kernel2 = (1/256)*np.array([[1, 4, 6, 4, 1],[4, 16, 24, 16, 4.0],[6, 24, 36, 24 , 6],[4, 16, 24, 16, 4],[1, 4, 6, 4 ,1]])
-kernel3 = np.array([[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1]])
+
+kernel_A = np.array([[256,256,256],[256,256,256],[256,256,256]])
+kernel_B = np.array([[250,250,250],[250,250,250],[250,250,250]])
+kernel_C = np.array([[0,0,0],[0,0,0],[0,0,0]])
+kernel_D = (1/256)*np.array([[1, 4, 6, 4, 1],[4, 16, 24, 16, 4.0],[6, 24, 36, 24 , 6],[4, 16, 24, 16, 4],[1, 4, 6, 4 ,1]])
+kernel_E = np.array([[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1],[1,2,0,-2,-1]])
 img = io.imread('leena512.bmp',as_gray=True)  # load the image as grayscale
 
 def convolution(img,kernel):
@@ -21,10 +24,19 @@ def convolution(img,kernel):
             output[y, x] = (kernel_rotated * img[y:y + len(kernel), x:x + len(kernel)]).sum()
     return output
 
-if __name__ == '__main__':
+
+def graph(img, title, ylabel, xlabel):
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
     plt.imshow(img, interpolation='nearest', cmap='gray')
     plt.show()
 
-    plt.imshow(convolution(img,kernel3), interpolation='nearest',cmap='gray')
-    plt.show()
+
+if __name__ == '__main__':
+    graph(convolution(img, kernel_A),"Filtro A","Pixel","Pixel")
+    graph(convolution(img, kernel_B), "Filtro B", "Pixel", "Pixel")
+    graph(convolution(img, kernel_C), "Filtro C", "Pixel", "Pixel")
+    graph(convolution(img, kernel_D), "Filtro D", "Pixel", "Pixel")
+    graph(convolution(img, kernel_D), "Filtro E", "Pixel", "Pixel")
 
