455b47f2064e99da1ed6cee19bb02fdc811aa3d1
mcoronado <matias.coronado@usach.cl>
2020-01-07 23:17:54 -0300

    Matrix order

--- lab3.py
+++ lab3.py
@@ -2,23 +2,33 @@ from skimage import io
 from matplotlib import pyplot as plt
 import numpy as np
 kernel = np.array([[0, -1, 0],[-1, 5, -1],[0, -1, 0]])
-kernel2 = np.array([[1/256, 4/256, 6/256, 4.0/256 ,1.0/256],[4.0/256, 16.0/256, 24.0/256, 16.0/256, 4.0/256],[6.0/256, 24.0/256, 36.0/256, 24.0/256 , 6.0/256],[4.0/256, 16.0/256 ,24.0/256, 16.0/256 ,4.0/256],[1/256, 4/256, 6/256, 4.0/256 ,1.0/256]])
-img = io.imread('leena512.bmp',as_gray=True)  # load the image as grayscale
+kernel2 = np.array(
+    [
+        [1.0/256, 4.0/256, 6.0/256, 4.0/256 ,1.0/256],
+        [4.0/256, 16.0/256, 24.0/256, 16.0/256, 4.0/256],
+        [6.0/256, 24.0/256, 36.0/256, 24.0/256 , 6.0/256],
+        [4.0/256, 16.0/256 ,24.0/256, 16.0/256 ,4.0/256],
+        [1.0/256, 4.0/256, 6.0/256, 4.0/256 ,1.0/256]
+    ]
+)
 
+img = io.imread('leena512.bmp',as_gray=True)  # load the image as grayscale
 
 output = np.zeros_like(img)
 
+largo = len(kernel2)
 
-image_padded = np.zeros((img.shape[0]+2, img.shape[1]+2))
+image_padded = np.zeros((img.shape[0]+largo-1, img.shape[1]+largo-1))
 
 print("SHAPE 0 ",img.shape[0])
 print("SHAPE 0 ",img.shape[1])
 
 image_padded[1:-1, 1:-1] = img
 
-for x in range(img.shape[1]):     # Loop over every pixel of the image
+for x in range(img.shape[1]):
         for y in range(img.shape[0]):
-            output[y, x] = (kernel * image_padded[y:y + 3, x:x + 3]).sum()
+            output[y, x] = (kernel2 * image_padded[y:y + largo, x:x + largo]).sum()
+
 
 print(image_padded)
 print(output)
