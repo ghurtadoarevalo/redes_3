8cdbb1af092fc1f9775610c7a951d5875d3d2c89
Gustavo H <gustavo.hurtado@usach.cl>
2020-01-08 00:12:03 -0300

    convolution working

--- lab3.py
+++ lab3.py
@@ -1,30 +1,29 @@
 from skimage import io
 from matplotlib import pyplot as plt
 import numpy as np
-kernel = np.array([[0, -1, 0],[-1, 5, -1],[0, -1, 0]])
-kernel2 = np.array([[1/256, 4/256, 6/256, 4.0/256 ,1.0/256],[4.0/256, 16.0/256, 24.0/256, 16.0/256, 4.0/256],[6.0/256, 24.0/256, 36.0/256, 24.0/256 , 6.0/256],[4.0/256, 16.0/256 ,24.0/256, 16.0/256 ,4.0/256],[1/256, 4/256, 6/256, 4.0/256 ,1.0/256]])
+kernel = np.array([[256,256,256],[256,256,256],[256,256,256]])
+kernel2 = (1/256)*np.array([[1, 4, 6, 4, 1],[4, 16, 24, 16, 4.0],[6, 24, 36, 24 , 6],[4, 16, 24, 16, 4],[1, 4, 6, 4 ,1]])
 img = io.imread('leena512.bmp',as_gray=True)  # load the image as grayscale
 
+def convolution(img,kernel):
+    output = np.zeros_like(img)
 
-output = np.zeros_like(img)
+    image_padded = np.zeros((img.shape[0] - kernel.shape[0] + 1, img.shape[1] - kernel.shape[1] + 1))
 
+    # Se rota el kernel
+    kernel_rotated = np.flipud(np.fliplr(kernel))
 
-image_padded = np.zeros((img.shape[0]+2, img.shape[1]+2))
+    # image_padded[1:-1, 1:-1] = img
 
-print("SHAPE 0 ",img.shape[0])
-print("SHAPE 0 ",img.shape[1])
+    for x in range(kernel.shape[0] - 1, image_padded.shape[1]):  # Loop over every pixel of the image
+        for y in range(kernel.shape[0] - 1, image_padded.shape[0]):
+            output[y, x] = (kernel_rotated * img[y:y + len(kernel), x:x + len(kernel)]).sum()
+    return output
 
-image_padded[1:-1, 1:-1] = img
+if __name__ == '__main__':
+    plt.imshow(img, interpolation='nearest', cmap='gray')
+    plt.show()
 
-for x in range(img.shape[1]):     # Loop over every pixel of the image
-        for y in range(img.shape[0]):
-            output[y, x] = (kernel * image_padded[y:y + 3, x:x + 3]).sum()
+    plt.imshow(convolution(img,kernel2), interpolation='nearest',cmap='gray')
+    plt.show()
 
-print(image_padded)
-print(output)
-
-plt.imshow(kernel, interpolation='nearest',cmap='gray')
-plt.show()
-
-plt.imshow(kernel2, interpolation='nearest',cmap='gray')
-plt.show()
